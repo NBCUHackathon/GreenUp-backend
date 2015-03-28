bf6120cdce911f1a8c576c650c82e2dd9a736c4b
tylerl0706 <tylerl0706@gmail.com>
2015-03-28 16:32:46 -0400

    init commit

---
+++ server.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+// var io = require('../..')(server);
+// New:
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3000;

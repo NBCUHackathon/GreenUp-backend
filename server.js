f10f0605a77a0ceee3437568b2a4e3fc83aad6e7
tylerl0706 <tylerl0706@gmail.com>
2015-03-28 23:28:31 -0400

    stuff and things

--- server.js
+++ server.js
@@ -6,17 +6,63 @@ var io = require('socket.io')(server);
 var mongojs = require('mongojs');
 var request = require('request');
 
-var port = process.env.PORT || 3000;
+// var port = process.env.PORT || 3000;
+var port = 3000;
+
+var uri = "mongodb://master:golf@ds062097.mongolab.com:62097/golf";
+var golferDB = mongojs(uri, ['golfer_reservation_requests']);
+var courseDB = mongojs(uri, ['course_reservation_requests']);
+
 
 app.get('/', function(req, res){
-  res.send('hello world');
+
+	golferDB.golfer_reservation_requests.find({},function(err,docs){
+
+		var html = "";
+
+		for(var i = 0; i<docs.length; i++){
+			docs[i].reservation_requests.forEach(function(reserve){
+
+				html = html + "<div>person: " + docs[i].token + "</div><br/><div>reservation: " + reserve.name + " " + reserve.date + " " + reserve.time + "</div><br/><br/>"
+
+			});
+		}
+
+		// docs.forEach(function(people){
+
+		// 	people.reservation_requests.forEach(function(reserve){
+
+		// 		html = html + "<div>person: " + people.token + "</div><br/><div>reservation: " + reserve.FacilityID + " " + reserve.Time + "</div><br/><br/>"
+
+		// 	});
+
+		// });
+		res.send(html);
+	});
+	// res.send('hello world');
 });
 
 app.listen(port);
 
-var uri = "mongodb://pacific-brook-1:HackMCO2015@ds062097.mongolab.com:62097/golf";
-var golferDB = mongojs(uri, ['golfer_reservation_requests']);
-var courseDB = mongojs(uri, ['course_reservation_requests']);
+console.log(port);
+
+
+request.get(
+		    {
+		    	uri:'https://sandbox.api.gnsvc.com/rest/channel/17652/facilities?q=geo-location&latitude=28.4158&longitude=-81.2989&proximity=25&expand=FacilityDetail.Ratesets',
+		     headers: { 'UserName': "Hackathon_Development",
+		              	'Password': "6YBkHF86ut7946pDwZhp",
+		          		"Access-Control-Allow-Origin": "*"} 
+		 },
+		    function (error, response, body) {
+		        if (!error && response.statusCode == 200) {
+		            // console.log(body);
+		            filterTeeTimes(JSON.parse(body), {startTime:'08:00:00', endTime:'11:00:00', token:'hi', date:"2015-03-29", price:"$12.50"});
+
+
+		        }
+		    }
+		);
 
 
 // //while connected to a client
@@ -25,7 +71,21 @@ io.on('connection', function(socket) {
 	socket.on('reservation.sent', function(data){
 
 
-
+		//sends the yo back with a link
+		request.get(
+		    {
+		    	uri:'https://sandbox.api.gnsvc.com/rest/channel/17652/facilities?q=geo-location&latitude=28.4158&longitude=-81.2989&proximity=25&expand=FacilityDetail.Ratesets',
+		     headers: { 'UserName': "Hackathon_Development",
+		              	'Password': "6YBkHF86ut7946pDwZhp",
+		          		"Access-Control-Allow-Origin": "*"} 
+		 },
+		    function (error, response, body) {
+		        if (!error && response.statusCode == 200) {
+		            console.log(body);
+		            filterTeeTimes(body, data);
+		        }
+		    }
+		);
 
 
 
@@ -36,9 +96,123 @@ io.on('connection', function(socket) {
 	});
 
 
-	socket.on('reservations.get.golfer', function(data){});
+	socket.on('reservations.get.golfer', function(data){
+
+		golferDB.golfer_reservation_requests.findOne({"token":socket.username}, function(doc){
+
+			sendSorted(doc,socket);
+
+		});
+
+
+	});
 
 
 	socket.on('reservations.get.course', function(data){});
 
+	socket.on('receive.token', function(data){
+
+		socket.username = data;
+		var temp = {
+			"token":data,
+			"reservation_requests":[]
+		}
+		golferDB.golfer_reservation_requests.save(temp);
+
+
+	});
+
 });
+
+function filterTeeTimes(getReq, userData){
+	
+	getReq.forEach(function (golfCourse) {
+
+		golfCourse.Ratesets.Value.forEach(function (teeTime){
+			teeTime.status = "pending";
+			teeTime.name = golfCourse.Name;
+			// console.log("a");
+			var date = teeTime.Time.split('T')[0];
+			var time = teeTime.Time.split('T')[1];
+
+			if(date === userData.date){
+				// console.log("b");
+				var courseTimeSplit = time.split(':');
+				var golferStartTimeSplit = userData.startTime.split(':');
+				var golferEndTimeSplit = userData.endTime.split(':');
+
+				var courseSeconds = (courseTimeSplit[0]*60*60)+(courseTimeSplit[1]*60)+courseTimeSplit[2];
+				var golferStartSeconds = (golferStartTimeSplit[0]*60*60)+(golferStartTimeSplit[1]*60)+golferStartTimeSplit[2];
+				var golferEndSeconds = (golferEndTimeSplit[0]*60*60)+(golferEndTimeSplit[1]*60)+golferEndTimeSplit[2];
+
+				if(courseSeconds >= golferStartSeconds && courseSeconds <= golferEndSeconds){	
+					// console.log("mongo");
+					var temp = {
+						status:"pending",
+						name:golfCourse.Name,
+						price:userData.price,
+						date:teeTime.Time.split('T')[0],
+						time:teeTime.Time.split('T')[1]
+					}
+
+					golferDB.golfer_reservation_requests.update({"token":userData.token},{ $push: {"reservation_requests":temp}},
+			        	{new:true}
+			        ,function(err, doc){
+			        	// console.log("help"+doc);
+
+
+			      
+
+			        });
+
+					//golferDB.golfer_reservation_requests.update({username:data.username}, {'$inc':{'clues':1}}, {'upsert':true});
+					// IF WE HAVE TIME TO HAVE ANOTHER DB FOR COURSES
+			        // courseDB.course_reservation_requests.findAndModify({
+			        //     query: {token:userData.token},
+			        //     update: { $push: {"reservation_requests":teeTime},
+			        // 	upsert: true}
+			        // });
+
+				}
+
+			}else{
+
+			}
+		});
+
+	});
+
+}
+
+function sendSorted(data,socket){
+
+	var accepted = [];
+	var pending = [];
+	var declined = [];
+
+	// golferDB.golfer_reservation_requests.findOne({"token":"hi"}, function(doc){
+
+	data.reservation_requests.forEach(function(tee){
+
+		if(tee.status === "accepted"){
+			accepted.push(tee);
+		}
+
+		if(tee.status === "pending"){
+			pending.push(tee);
+		}
+
+		if(tee.status === "declined"){
+			declined.push(tee);
+		}
+
+	});
+
+
+	socket.emit('send.reservations.golfer.accepted', accepted);
+	socket.emit('send.reservations.golfer.pending', pending);
+	socket.emit('send.reservations.golfer.declined', declined);
+
+						// });
+
+}

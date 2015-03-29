d62b1edb923d59d51fa0bbd8f17dd6881095be6c
tylerl0706 <tylerl0706@gmail.com>
2015-03-29 06:58:21 -0400

    twil

--- app.js
+++ app.js
@@ -372,7 +372,7 @@ function twil() {
 
 client.sms.messages.create({
     body: "All in the game, yo",
-    to: "+14108675309",
+    to: "+14075908293",
     from: "+15005550006"
 }, function(err, sms) {
     // process.stdout.write(sms.sid);
